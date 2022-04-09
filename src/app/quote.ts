6ef15c2459e0a6ea65da60c240479d1316446f03
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-04-09 23:02:06 +0300

    declare class variables

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,12 @@
 export class Quote {
+    id: number;
+    quote: string;
+    downvotes: number;
+    upvotes: number;
     constructor(id:number,quote:string,downvotes:number,upvotes:number){
-        
+        this.id=id;
+        this.quote=quote;
+        this.downvotes=downvotes;
+        this.upvotes=upvotes;
     }
 }
