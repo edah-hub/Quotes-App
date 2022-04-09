3e7ba5902af6a8bcc21b42de0fb57f7e01c250d5
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-04-10 00:30:18 +0300

    creates a directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
