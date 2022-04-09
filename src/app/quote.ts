c45b5a0a9023a5a235f9bb665c17871844df46d8
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-04-09 23:46:05 +0300

    adds a list of quotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,12 +1,29 @@
+// export class Quote {
+//     id: number;
+//     quote: string;
+//     downvotes: number;
+//     upvotes: number;
+//     constructor(id:number,quote:string,downvotes:number,upvotes:number){
+//         this.id=id;
+//         this.quote=quote;
+//         this.downvotes=downvotes;
+//         this.upvotes=upvotes;
+//     }
+// }
+
+
 export class Quote {
-    id: number;
-    quote: string;
-    downvotes: number;
-    upvotes: number;
-    constructor(id:number,quote:string,downvotes:number,upvotes:number){
-        this.id=id;
-        this.quote=quote;
-        this.downvotes=downvotes;
-        this.upvotes=upvotes;
+    public showDetails: boolean;
+    constructor(
+      public  id: number,
+      public quote: string,
+      public authorName: string,
+      public submittedBy: string,
+      public submissionDate: Date,
+      public upvote: number,
+      public downvote: number
+    ){
+      this.showDetails=false;
     }
-}
+  }
+  
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  // ngOnInit(): void {
+  //   throw new Error('Method not implemented.');
+  // }
+  quotes:Quote[] = [
+  
+    new Quote(1,'The technology you use impresses no one. The experience you create with it is everything.', 'Sean Gerety', 'ECC@04', new Date(2019, 0, 6), 0, 0),
+    new Quote(2, 'Life is what happens when you’re busy making other plans.', 'John Lennon', 'Joe1', new Date(2016, 2,8), 0, 0),
+    new Quote (3, 'Many of life’s failures are people who did not realize how close they were to success when they gave up.', 'Thomas A. Edison', 'Blue Ivy', new Date(2017, 1, 5), 0, 0),
+    new Quote (4, 'You only live once, but if you do it right, once is enough.', 'Mae West', 'Unit2', new Date (2020, 0, 1), 0, 0),
+    new Quote (5, 'The big lesson in life, baby, is never be scared of anyone or anything.', 'Frank Sinatra','user04', new Date (2020, 2, 27), 0, 0),
+    new Quote (6, 'Get busy living or get busy dying', 'Stephen King', 'User', new Date (2019, 6, 25),0, 0)
+  ]
+ 
+ 
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+function constructor() {
+  throw new Error('Function not implemented.');
+}
+
+function ngOnInit() {
+  throw new Error('Function not implemented.');
+}
+
